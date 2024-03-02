67a2d06dab95182562a0923a5eb83332115482e0
nielfk <121296880+nielfk@users.noreply.github.com>
2024-03-02 17:56:59 +0300

    second commit
    # Conflicts:
    #	README.md

---
+++ lesson7/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import PokemonPage from './page/PokemonPage/PokemonPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CountPage from './page/countPage/CountPage';
+import Users from './page/users/Users';
+import Menu from './components/menu/Menu';
+import UserInfo from './page/UserInfo/UserInfo';
+import FormPage from './page/FormPage/FormPage';
+
+
+function App() {
+
+
+    return (
+        <BrowserRouter>
+            <Menu/>
+            <Routes>
+                <Route path='/' element={<PokemonPage/>}/>
+                <Route path='/count' element={<CountPage/>}/>
+                <Route path='/users' element={<Users/>}/>
+                <Route path='/form' element={<FormPage/>}/>
+                <Route path='/users/:id' element={<UserInfo/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+
+export default App;
